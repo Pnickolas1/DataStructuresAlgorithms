6fe7c2159f3258c6a25078260a97163ca9618dd4
Eleanor Mark <eleanormark@gmail.com>
2017-10-09 14:49:38 -0700

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  });
+
+  return lettersArr.join('') === lettersArr.reverse().join('');
+}
+
+console.log(isPalindrome("Madam, I'm Adam"));
+console.log(isPalindrome('race car'));
+console.log(isPalindrome('javascript the good part'));
+
+//next exercise js regularEx solution

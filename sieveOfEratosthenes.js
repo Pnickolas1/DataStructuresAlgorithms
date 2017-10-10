e7dc50edb64eb8242deb8d26492a71c4427636cc
Eleanor Mark <eleanormark@gmail.com>
2017-10-10 14:57:44 -0700

    prettify

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -2,25 +2,24 @@
 // return all prime number up to 'num' in an array
 
 function sieveOfEratosthenes(num) {
-    
-    var primes = [];
-    for (var i = 0; i<= num; i++) {
-      primes[i] = true;
-    }
-    primes[0] = false;
-    primes[1] = false;
-    
-    for (var i = 2; i <= Math.sqrt(num); i++){
-      for (var j = 2; j * i <= num; j++) {
-        primes[i*j] = false;
-      }
-    }
-    var result =[];
-    for(var k = 0; k < primes.length; k++) {
-      if(primes[k]) result.push(k);
-    }
-    return result;
+  var primes = [];
+  for (var i = 0; i <= num; i++) {
+    primes[i] = true;
   }
-  
-  sieveOfEratosthenes(20);
-  //should return [2, 3, 5, 7, 11, 13, 17, 19]
+  primes[0] = false;
+  primes[1] = false;
+
+  for (var i = 2; i <= Math.sqrt(num); i++) {
+    for (var j = 2; j * i <= num; j++) {
+      primes[i * j] = false;
+    }
+  }
+  var result = [];
+  for (var k = 0; k < primes.length; k++) {
+    if (primes[k]) result.push(k);
+  }
+  return result;
+}
+
+sieveOfEratosthenes(20);
+//should return [2, 3, 5, 7, 11, 13, 17, 19]
